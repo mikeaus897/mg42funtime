e4d47a3281324a77b117272f3344f3b0f0f9630e
mikeaus897 <93657413+mikeaus897@users.noreply.github.com>
2022-01-13 17:40:42 +1000

    Update g_players.c

--- src/game/g_players.c
+++ src/game/g_players.c
@@ -31,7 +31,7 @@ void Cmd_GetStatus_f(gentity_t *ent) {
 	}
 
 	CP(va("print \"\n^3Mod: ^7%s \n^3Server: ^7%s\n%s\"", GAMEVERSION, GetHostname(), server_ip_with_port ? server_ip_with_port : ""));
-        CP("print \"^3Game physics: ^7Vanilla\n\"");
+	CP("print \"^3Game physics: ^7Vanilla\n\"");
 	CP("print \"^3-----------------------------------------------------------------------------\n\"");
 	CP("print \"^7CN : Name            : ^3IP              ^7: Country         : Ping ^7: Status     \n\"");
 	CP("print \"^3-----------------------------------------------------------------------------\n\"");
