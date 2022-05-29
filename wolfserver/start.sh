a78aec97e1a7ec258d7a9e9a7142dffa75314e29
mikeaus897 <93657413+mikeaus897@users.noreply.github.com>
2022-05-29 11:52:17 +1000

    updated start.sh script
    chmod +x start.sh
    ./start.sh

--- wolfserver/start.sh
+++ wolfserver/start.sh
@@ -1,1 +1,5 @@
-./wolfded.x86 +set fs_game rtcwPub +exec deathmatch.cfg
+# for MG server:
+./wolfded.x86 +set fs_homepath "/root/.wolf" +set fs_game rtcwPub +set dedicated 2 +set net_port 27960 +set vm_game 0 +set sv_pure 0 +set com_hunkmegs 64 +exec deathmatch.cfg
+
+# for DM server:
+# ./wolfded.x86 +set fs_homepath "/root/.wolf" +set fs_game rtcwPub +set dedicated 2 +set net_port 27960 +set vm_game 0 +set sv_pure 0 +set com_hunkmegs 200 +exec deathmatch.cfg
