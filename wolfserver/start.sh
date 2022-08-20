6254ed5a25c51802cbfe1b892c7ff15bf877fa20
mikeaus897 <93657413+mikeaus897@users.noreply.github.com>
2022-08-20 16:22:35 +1000

    update start.sh to match with /wolfserver folder
    update start.sh to match with /wolfserver folder

--- wolfserver/start.sh
+++ wolfserver/start.sh
@@ -1,5 +1,5 @@
 # for MG server:
-./wolfded.x86 +set fs_homepath "/root/.wolf" +set fs_game rtcwPub +set dedicated 2 +set net_port 27960 +set vm_game 0 +set sv_pure 0 +set com_hunkmegs 64 +exec deathmatch.cfg
+./wolfded.x86 +set fs_homepath "/root/wolfserver" +set fs_game rtcwPub +set dedicated 2 +set net_port 27960 +set vm_game 0 +set sv_pure 0 +set com_hunkmegs 64 +exec deathmatch.cfg
 
 # for DM server:
-# ./wolfded.x86 +set fs_homepath "/root/.wolf" +set fs_game rtcwPub +set dedicated 2 +set net_port 27960 +set vm_game 0 +set sv_pure 0 +set com_hunkmegs 200 +exec deathmatch.cfg
+# ./wolfded.x86 +set fs_homepath "/root/wolfserver" +set fs_game rtcwPub +set dedicated 2 +set net_port 27960 +set vm_game 0 +set sv_pure 0 +set com_hunkmegs 200 +exec deathmatch.cfg
